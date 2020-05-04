28625d0630e5dd7b767738f0a9ea43a06757fa50
Vic Dyomin <dyomin.vic@gmail.com>
2020-05-04 23:31:41 +0300

    Initial commit

---
+++ Backend/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebsocketApp.Controllers
+{
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResponse Get()
+        {
+            return Ok("Websocket App");
+        }
+    }
+}
